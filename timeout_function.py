fdfb67019f6059f2a2211cd42e7c364863051b8c
XTF <nkxy007@gmail.com>
2023-11-04 17:49:00 +1100

    adding timeout to translators

--- timeout_function.py
+++ timeout_function.py
@@ -2,6 +2,8 @@ import signal
 import time
 import sys
 import multiprocessing
+from typing import Callable
+
 
 class TimeoutError(Exception):
     pass
@@ -9,13 +11,15 @@ class TimeoutError(Exception):
 def timer_handler(signum, frame):
     raise TimeoutError
 
-def beautiful_function(sleep_time=1):
+def beautiful_function(q, sleep_time=1):
     print(f"going to sleep for {sleep_time}")
     time.sleep(sleep_time)
     print("finished sleeping")
+    if q:
+        q.put(sleep_time)
 
 
-def timeout_tester_linux(sleep_time):
+def timeout_tester_linux(sleep_time, target_function:Callable):
     """
     Signals work with linux so this function only runs on linux
     """
@@ -23,12 +27,13 @@ def timeout_tester_linux(sleep_time):
     signal.alarm(3)
 
     try:
-        beautiful_function(sleep_time)
+        target_function(q=None, sleep_time=sleep_time)
     except TimeoutError:
-        print("sanctioned functi cause it sleeped to long")
+        print("sanctioned function cause it sleeped to long")
 
-def timeout_tester_windows(sleep_time):
-    p = multiprocessing.Process(target=beautiful_function, args=[sleep_time])
+def timeout_tester_windows(sleep_time: int, target_function:Callable):
+    q = multiprocessing.Queue()
+    p = multiprocessing.Process(target=target_function, args=[q, sleep_time])
     p.start()
     #timeout after 3 seconds
     p.join(3)
@@ -38,12 +43,15 @@ def timeout_tester_windows(sleep_time):
             p.join()
             raise TimeoutError()
     except TimeoutError:
-        print(f"sanctioned functi cause it sleeped to long: {p.exitcode=}")
+        print(f"sanctioned function {target_function.__name__} cause it sleeped too long: {p.exitcode}")
+        return
+    result = q.get_nowait()
+    print(f"result: {result}")
     return p.exitcode
 
 if __name__ == "__main__":
-    if sys.platform == "linuxi":
-        timeout_tester_linux(4)
-    elif sys.platform == "linux": # "win32"
-        timeout_tester_windows(5)
+    if sys.platform == "linux":
+        timeout_tester_linux(4, beautiful_function)
+    elif sys.platform == "win32":
+        timeout_tester_windows(5, beautiful_function)
 
--- translator.py
+++ translator.py
@@ -6,12 +6,47 @@
 import whisper
 import os
 from time import sleep
+import openai
+from typing import Any, Callable
+import multiprocessing
 
 LANGUAGE = 'french'
 processed_files = []
 model = whisper.load_model("base")
 original_text_list = []
 translated_text_list = []
+
+
+def timeout_try_windows(audio_file: Any, model: Any, target_function:Callable):
+    # create a queue to store result of the translated text
+    q = multiprocessing.Queue()
+    p = multiprocessing.Process(target=target_function, args=[audio_file, model, q])
+    p.start()
+    #timeout after 2 seconds
+    p.join(2)
+    try: 
+        if p.is_alive():
+            p.terminate()
+            p.join()
+            raise TimeoutError()
+    except TimeoutError:
+        print(f"sanctioned function {target_function.__name__} cause it sleeped too long: {p.exitcode}")
+        print(p.exitcode)
+        return {"text": ("nan","nan")}
+    result = q.get_nowait()
+    return {"text":result}
+
+def translategpt(text):
+    translated_text = openai.ChatCompletion("davinci", prompt="", text=text)
+    return translated_text
+
+
+def translate_speech(audio_data, model, queue):
+    transcribed = model.transcribe(audio_data, language=LANGUAGE)
+    translated = translategpt(result)
+    queue.put(transcribed, translated)
+    return (transcribed, translated)
+
 while True:
     # get files
     files = [files for x,y, files in os.walk(".")][0]
@@ -23,11 +58,12 @@ while True:
             print(f"processing {file}")
             # initiste this as a timed process that can not last more than 2 sec
             result = model.transcribe(file, language=LANGUAGE)
-            # translate = openai.translate(result)
-            original_text_list.append(result["text"])
-            print(f'original text: {result["text"]} \n')
-            # print(f'translated: {translate} \n')
-            # translated_text_list.append(translate)
+            result = timeout_try_windows(file, model, translate_speech)
+            original_text_list.append(result["text"][0])
+            print(f'original text: {result["text"][0]} \n')
+            translate = result["text"][1]
+            print(f'translated: {translate} \n')
+            translated_text_list.append(translate)
             processed_files.append(file)
 		
     else:
